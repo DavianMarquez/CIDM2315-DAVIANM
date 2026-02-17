098dc3aac91c11aedb4a1e95af01580b5bc49497
DavianMarquez <djmarquez1@buffs.wtamu.edu>
2026-02-17 00:09:18 -0600

    Add files via upload
    Code for Homework4

---
+++ Homework4.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Permissions;
+
+namespace Homework4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 3;
+        int b = 5;                      //code for q1
+        int largest = Largest(a, b);
+
+        Console.WriteLine("a = " + a + "; b=" +b);
+        Console.WriteLine("The largest number is: " + largest);
+        
+        Console.WriteLine();
+
+        //code for q2
+        PrintTriangle(5, "left");
+        Console.WriteLine();
+        PrintTriangle(5, "right");
+    }
+    //code for q1
+    static int Largest(int x, int y)
+    {
+        if (x > y)
+            return x;
+        else
+            return y;
+    }
+
+    //code for q2
+    static void PrintTriangle(int n, string shape)
+    {
+        Console.WriteLine("N is: " + n + "; shape is " + shape);
+        if(shape.ToLower() == "left")
+        {
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(new string('*', i));
+            }
+        }
+        else if(shape.ToLower() == "right")
+        {
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(new string(' ', n - i) + new string('*', i));
+            }
+        }
+    }
+}
